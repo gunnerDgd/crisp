2bd5a73f0cb2584b18eac967fdf3bd8abe3cb826
gunnerDgd <ifstudiomail@naver.com>
2022-11-26 18:16:56 +0900

    SPSC Example Code Added !!

--- arch/x86_64/spsc_msvc.asm
+++ arch/x86_64/spsc_msvc.asm
@@ -8,7 +8,7 @@ global __spsc_pop_until_success
 section .text
 
 ;crisp_bool (a.k.a char)
-;	__spsc_do_push
+;	__spsc_push
 ;		(__spsc_head*, void*);
 
 ; RCX : (__spsc_head*) pHead
@@ -21,9 +21,8 @@ __spsc_push:
     cmp rax, qword[rcx + 0x10] ; Compare if Write Pointer is same with Read Pointer.
     je  __push_failed
 
-    mov rax, qword[rcx + 0x08]
-    mov      qword[rax + 0x10], rdx 
-    cmpxchg  qword[rcx + 0x08], r9
+    mov     qword[rax + 0x10], rdx
+    cmpxchg qword[rcx + 0x08], r9
 
     mov rax, 1 ; Set Return Value as True
     ret
@@ -46,23 +45,22 @@ __spsc_push_until_success:
     cmp rax, qword[rcx + 0x10]    ; Compare if Write Pointer is same with pop_ptr.
     je __spsc_push_until_success
 
-    mov rax, qword[rcx + 0x08]
     mov      qword[rax + 0x10], rdx
     cmpxchg  qword[rcx + 0x08], r9
 
     ret
 
 ;void* (a.k.a char)
-;	__spsc_do_pop
+;	__spsc_pop
 ;		(__spsc_head*);
 
 ; RCX : (__spsc_head*)pHead
 
 __spsc_pop:
-    mov rax, qword[rcx + 0x10]      ; pHead->pop_ptr      (RAX)
-    mov r9 , qword[rax]             ; pHead->pop_ptr->next (R9)
+    mov rax, qword[rcx + 0x10]     ; pHead->pop_ptr       (RAX)
+    mov r9 , qword[rax]            ; pHead->pop_ptr->next (R9)
 
-    cmp r9, qword[rcx + 0x08] ; Compare if Next of Read Pointer is same with Write Pointer.
+    cmp r9, qword[rcx + 0x08]      ; Compare if Next of Read Pointer is same with Write Pointer.
     je  __pop_failed
 
     cmpxchg  qword[rcx + 0x10], r9 ; pHead->pop_ptr = pHead->pop_ptr_next (advance)
@@ -74,16 +72,16 @@ __pop_failed:
     ret
 
 ;void*
-;	__spsc_do_pop_until_success
+;	__spsc_pop_until_success
 ;		(__spsc_head*);
 
 ; RCX : (__spsc_head*) pHead
 
 __spsc_pop_until_success:
-    mov rax, qword[rcx + 0x10]      ; pHead->pop_ptr      (RAX)
-    mov r9 , qword[rax]             ; pHead->pop_ptr->next (R9)
+    mov rax, qword[rcx + 0x10] ; pHead->pop_ptr      (RAX)
+    mov r9 , qword[rax]        ; pHead->pop_ptr->next (R9)
 
-    cmp r9, qword[rcx + 0x08] ; Compare if Next of Read Pointer is same with Write Pointer.
+    cmp r9, qword[rcx + 0x08]  ; Compare if Next of Read Pointer is same with Write Pointer.
     je  __spsc_pop_until_success
 
     cmpxchg  qword[rcx + 0x10], r9 ; pHead->pop_ptr = pHead->pop_ptr_next (advance)
---
+++ examples/atomic/spsc.cpp
@@ -0,0 +1,54 @@
+extern "C"
+{
+#include <crisp/atomic/spsc.h>
+#include <crisp/queue/queue.h>
+
+#include <crisp/allocator/standard_library.h>
+}
+
+#include <iostream>
+#include <thread>
+
+void fn_producer(crisp_spsc* spsc) {
+	for(crisp_u64 push_count  = 1		    ;
+				  push_count <= 1000 * 1000 ;
+				  push_count++)			    {
+		crisp_u64*							 push_value = new crisp_u64(push_count);
+		crisp_queue_push_until_success(spsc, push_value);
+	}
+}
+
+void fn_consumer(crisp_spsc* spsc, crisp_u64& sum) {
+	for(crisp_u64 pop_count = 0			 ;
+				  pop_count < 1000 * 1000;
+				  pop_count++)		     {
+		crisp_u64 pop_value =  *(crisp_u64*)crisp_queue_pop_until_success(spsc);
+		sum += pop_value;
+	}
+}
+
+int main() {
+	crisp_spsc		spsc;
+	crisp_allocator alloc;
+
+	std::thread*	producer,
+			   *	consumer;
+
+	crisp_u64		sum = 0;
+
+	std::cout << "[EXAMPLE] crisp::atomic::spsc" << std::endl;
+
+	crisp_allocator_standard_library(&alloc);
+	crisp_spsc_initialize		    (&spsc, &alloc, 1000 * 1000 + 1);
+
+	producer = new std::thread(fn_producer, &spsc);
+	consumer = new std::thread(fn_consumer, &spsc, std::ref(sum));
+	
+	producer->join();
+	consumer->join();
+
+	std::cout << "[SUM] " << sum << std::endl;
+
+	crisp_spsc_cleanup(&spsc);
+
+}
