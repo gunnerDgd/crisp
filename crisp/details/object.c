1fad3824440f09f548ab26c8d2089213e974a3e6
gunnerDgd <ifstudiomail@naver.com>
2023-08-02 05:38:21 +0900

    [Update] Object Trait Added - For Callback on Init / Clone / Reference / Deinit

--- crisp/details/object.c
+++ crisp/details/object.c
@@ -2,13 +2,15 @@
 #include "atomic.h"
 
 object_t*
-    object_init(alloc_t* par_alloc, c_u64_t par_alloc_size) {
+    object_init(alloc_t* par_alloc, object_trait_t* par_trait, c_u64_t par_alloc_size) {
         mem_t    *object_mem = mem_init(par_alloc, par_alloc_size + sizeof(object_t));
         object_t *object     = mem_ptr (object_mem);
 
                   object->mem   = object_mem;
                   object->ref   =          1;
                   object->alloc =  par_alloc;
+                  object->trait =  par_trait;
+
         return    object;
 }
 
@@ -20,6 +22,8 @@ object_t*
                   object->mem   =              object_mem;
                   object->ref   =                       1;
                   object->alloc = par_object_clone->alloc;
+
+                  object->trait->init_as_clone(mem_ptr(object_mem) + sizeof(object_t), mem_ptr(par_object_clone->mem) + sizeof(object_t));
         return    object;
 }
 
@@ -28,6 +32,7 @@ object_t*
         c_u64_t ref;
         do { ref = par_object->ref; } while(atomic_cmpxchg_u64(&par_object->ref, ref + 1) != ref);
 
+               par_object->trait->init_as_ref(mem_ptr(par_object->mem) + sizeof(object_t));
         return par_object;
 }
 
@@ -37,6 +42,7 @@ c_bool_t
         do { ref = par_object->ref; } while(atomic_cmpxchg_u64(&par_object->ref, ref - 1) != ref);
 
         if(ref == 0) {
+            par_object->trait->deinit(mem_ptr(par_object->mem) + sizeof(object_t));
             mem_deinit(par_object->mem);
             return true;
         }
