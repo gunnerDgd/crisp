5aa63c147196ce7aec616bffc4c27f0a6a384aa0
gunnerDgd <ifstudiomail@naver.com>
2023-11-26 16:17:45 +0900

    [Refactor] Removed Platform - Dependent Features

--- src/endian.c
+++ src/endian.c
@@ -1,6 +1,6 @@
 #include "endian.h"
 
-#ifdef PRESET_X86_64
+#ifdef PRESET_ARCH_X86_64
 u16_t 
     be16(u16_t par) { 
         return ((par & 0xFF00) >> 8) | ((par & 0x00FF) << 8); 
@@ -16,15 +16,15 @@ u32_t
 }
 
 u64_t 
-    be64(u64_t par)       {
+    be64(u64_t par)           {
         u8_t   *ret = &par;
-        return (ret[7] << 56) | 
-               (ret[6] << 48) | 
-               (ret[5] << 40) | 
-               (ret[4] << 32) | 
-               (ret[3] << 24) | 
-               (ret[2] << 16) | 
-               (ret[1] << 8)  | 
+        return (ret[7] << 56) |
+               (ret[6] << 48) |
+               (ret[5] << 40) |
+               (ret[4] << 32) |
+               (ret[3] << 24) |
+               (ret[2] << 16) |
+               (ret[1] << 8)  |
                 ret[0];
 }
 
