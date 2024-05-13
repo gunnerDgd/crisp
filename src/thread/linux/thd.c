7057712ca40642fe5f1c1ad60971fb232ae1aef2
gunnerDgd <ifstudiomai@naver.com>
2024-05-13 23:28:41 +0900

    [Feathre] Module subsystem (mod_new / mod_del) added to thd (Thread)

--- src/thread/linux/thd.c
+++ src/thread/linux/thd.c
@@ -1,13 +1,17 @@
 #ifdef PRESET_FEATURE_THREAD
 #include "thd.h"
+#include "../../thread.h"
 
 void
     thd_do_run
-        (thd* self)                            {
-            if (trait_of(self) != thd_t) return;
-            if (this)                    return;
-            this = (struct this*) obj_new      (
-                null_t     ,
+        (thd* self)                                  {
+            if (trait_of(self) != thd_t) goto run_err;
+            if (!mod_new())              goto run_err;
+            if (this)                    goto run_err;
+
+            thread.this = self;
+            this = obj_new    (
+                null_t    ,
                 this_t    ,
                 2         ,
                 self->func,
@@ -16,7 +20,10 @@ void
 
             self->ret  = this->ret;
             self->stat = fut_ready;
+            mod_del();
             del(this);
+    run_err:
+            self->stat = fut_err;
 }
 
 u64_t
@@ -51,9 +58,9 @@ obj_trait *thd_t = &thd_trait;
 
 bool_t
     thd_new
-        (thd* self, u32_t par_count, va_list par)                            {
-            any_t func = null_t; if (par_count > 0) func = va_arg(par, any_t);
-            any_t arg  = null_t; if (par_count > 1) arg  = va_arg(par, any_t);
+        (thd* self, u32_t count, va_list par)                            {
+            any_t func = null_t; if (count > 0) func = va_arg(par, any_t);
+            any_t arg  = null_t; if (count > 1) arg  = va_arg(par, any_t);
             if (!func) return false_t;
             self->stat = fut_pend;
             self->func = func    ;
