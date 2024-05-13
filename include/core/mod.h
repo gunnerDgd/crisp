f03a0556d9dbc0d0a6784880d273fb6119d80e24
gunnerDgd <ifstudiomai@naver.com>
2024-05-13 13:35:14 +0900

    [Change] /core/mod.h - mod_do_new / mod_do_del added

--- include/core/mod.h
+++ include/core/mod.h
@@ -9,32 +9,45 @@ struct mod                 {
     struct obj       *mod  ;
 };
 
-#define dep(name) { name##_t, &name }
-#define use(...)              \
-    bool_t mod_new()         {\
-        struct mod mod[] =   {\
-            __VA_ARGS__      ,\
-            { null_t, null_t }\
-        };                    \
-        for (u64_t i = 0 ; ; ++i)   {\
-            if (!mod[i].trait) break;\
-            obj_new_at              (\
-                mod[i].mod  ,        \
-                mod[i].trait,        \
-                0                    \
-            );                       \
-        }             \
-        return true_t;\
-    }                 \
-    void mod_del()           {\
-        struct mod mod[] =   {\
-            __VA_ARGS__      ,\
-            { null_t, null_t }\
-        };                    \
-        for (u64_t i = 0 ; ; ++i)   {\
-            if (!mod[i].trait) break;\
-            obj_del(mod[i].mod);     \
-        }                            \
+bool_t
+    mod_do_new
+        (struct mod* mod)                          {
+            for (u64_t i = 0 ; mod[i].trait ; ++i) {
+                if (trait_of(mod[i].mod)) {
+                    ref (mod[i].mod);
+                    continue;
+                }
+
+                if (!obj_new_at(mod[i].mod,mod[i].trait, 0)) return false_t;
+            }
+
+            return true_t;
+}
+
+void
+    mod_do_del
+        (struct mod* mod)                          {
+            for (u64_t i = 0 ; mod[i].trait ; ++i) {
+                del (mod[i].mod);
+            }
+};
+
+#define dep(name) { (obj_trait*)name##_t, (obj*)&name }
+#define use(...)               \
+    bool_t mod_new()         { \
+        struct mod mod[] =   { \
+            __VA_ARGS__      , \
+            { null_t, null_t } \
+        };                     \
+        return mod_do_new(mod);\
+    }                          \
+                               \
+    void mod_del()           { \
+        struct mod mod[] =   { \
+            __VA_ARGS__      , \
+            { null_t, null_t } \
+        };                     \
+        mod_do_del(mod);       \
     }
 
 #endif
