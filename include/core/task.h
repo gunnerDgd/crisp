738851169d8b3d1c06fdedd276e347f522cd1be6
gunnerDgd <ifstudiomail@naver.com>
2024-06-07 08:59:35 +0900

    [Change] struct task : Resized (1 KB -> 2 MB for Stack Embedding)

--- include/core/task.h
+++ include/core/task.h
@@ -5,7 +5,7 @@
 #include "fut.h"
 
 extern obj_trait *task_t;
-typedef struct    task { u8_t task[1024]; } task;
+typedef struct    task { u8_t task[2 MB]; } task;
 
 fut* task_fut(task*);
 fut* async   (void*(*)(void*), void*);
