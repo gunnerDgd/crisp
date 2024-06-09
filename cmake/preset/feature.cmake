095356d35e2085af34f978948829c892215fca93
gunnerDgd <ifstudiomail@naver.com>
2024-06-10 02:14:08 +0900

    [Refactor] preset_(arch, oa, compiler) -> add_(...)

--- cmake/preset/feature.cmake
+++ cmake/preset/feature.cmake
@@ -1,14 +1,9 @@
-function   (enable_collections)
+function   (use_collections)
     set                    (PRESET_FEATURE_COLLECTIONS ON PARENT_SCOPE)
     add_compile_definitions(PRESET_FEATURE_COLLECTIONS)
 endfunction()
 
-function   (enable_libc)
-    set                    (PRESET_FEATURE_LIBC ON PARENT_SCOPE)
-    add_compile_definitions(PRESET_FEATURE_LIBC)
-endfunction()
-
-function   (enable_thread)
+function   (use_thread)
     set                    (PRESET_FEATURE_THREAD ON PARENT_SCOPE)
     add_compile_definitions(PRESET_FEATURE_THREAD)
 endfunction()
