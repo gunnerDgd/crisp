4a552d0715224ccf0aec47352a82f3fafe3828f5
gunnerDgd <ifstudiomail@naver.com>
2024-07-12 18:09:54 +0900

    [Fix] node->map

--- include/collections/map.h
+++ include/collections/map.h
@@ -1,18 +1,18 @@
-#ifndef __COLLECTIONS_MAP_H__
-#define __COLLECTIONS_MAP_H__
+#ifndef COLLECTIONS_MAP_H
+#define COLLECTIONS_MAP_H
 
 #include "pos.h"
 
 extern obj_trait* map_t;
 typedef struct    map  { u8_t map[256]; } map;
 
-node*  map_move (map*, obj*) ;
-node*  map_push (map*, obj*) ;
+pos*   map_move (map*, obj*) ;
+pos*   map_push (map*, obj*) ;
 void   map_pop  (map*, any_t);
-node*  map_find (map*, any_t);
+pos*   map_find (map*, any_t);
 
 bool_t map_empty(map*);
-node*  map_begin(map*);
-node*  map_end  (map*);
+pos*   map_begin(map*);
+pos*   map_end  (map*);
 
 #endif
